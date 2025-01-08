51037a975b7582a45a9bb5145607e1eabf95339c
deepadeepanjali <87965644+deepadeepanjali@users.noreply.github.com>
2025-01-08 15:05:46 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "azurerm" {
+  features {}
+}
+
+data "azurerm_key_vault" "keyvault" {
+  name                = var.keyvault 
+  resource_group_name = var.resourcegroup 
+}
+
+resource "azurerm_role_assignment" "secret_reader_role" {
+  principal_id       = var.principal_id  # Get Object ID of the AD group
+  role_definition_name = var.role_definition_name  # Assign the Secret Reader role
+  scope              = data.azurerm_key_vault.keyvault.id  # Assign to the entire Key Vault
+}
